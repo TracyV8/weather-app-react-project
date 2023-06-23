4d13d3b87e63b1d45b000d8201f412aecfb84296
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-06-23 13:29:25 +0800

    added Bootstrap in terminal

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Tracy Vonk and is
-        <a
-          href="https://github.com/TracyV8/weather-app-react-project"
-          target="-blank"
-        >
-          {" "}
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Tracy Vonk and is
+          <a
+            href="https://github.com/TracyV8/weather-app-react-project"
+            target="-blank"
+          >
+            {" "}
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
