d8454348c8c612680ace4394d8633cbe94dc73b8
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-06-23 13:10:39 +0800

    Added Github repository and link

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Tracy Vonk and is
+        <a
+          href="https://github.com/TracyV8/weather-app-react-project"
+          target="-blank"
+        >
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
