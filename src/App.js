4a7ba7419a6f056eded96cc6a46ec5bbcb0262f6
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-06-23 14:11:12 +0800

    Added initial HTML of Weather App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by Tracy Vonk and is
           <a
